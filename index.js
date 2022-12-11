e4f08543f8a7180999f50a4b5ec8af6e582a7df0
Suresh Kumar S <113002054+surae12@users.noreply.github.com>
2022-12-11 21:18:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-//json for loop
-var numbers = [10, 20, 30, 40, 50]
-for (var i = 0; i < numbers.length; i++) {
-    console.log(numbers[i])
+let myResume = {
+    "basics": {
+        "name": "Suresh Kumar S",
+        "email": "suricekmar12@gmail.com",
+        "phone": 7010755787,
+        "degree": "B.E",
+        "location": {
+            "address": "15/1-9A krishnappa Nagar",
+            "postalCode": 635109,
+            "city": "hosur",
+            "state": "Tamilnadu",
+            "country": "India"
+        },
+        "profiles": [
+            {
+                "website": "www.linkedin.com/in/suresh-kumar-s-2570a61a5",
+                "github": "https://github.com/surae12"
+            }
+        ]
+    },
+
+
+    "education": [
+        {
+            "institution": "Pavai College Of Technology",
+            "department": "Civil",
+            "studyType": "fulltime",
+            "batch start year": 2015,
+            "batch end year": 2020,
+            "cgpa": 6.25,
+        }
+    ],
+    "skills": [
+        {
+            "name": "html,css,Reactjs,javascript",
+            "level": "beginner",
+        }
+    ],
+    "languages": [
+        {
+            "language": "Tamil,English",
+        }
+    ],
+    "interests": [
+        {
+            "name": "coding,finding solutions,cooking,playing,",
+        }
+    ]
 }
-
-//for in loop
-
-var person = {
-    fname: "Nick",
-    lname: "Jonas",
-    age: 26
-};
-for (let x in person) {
-    console.log(x + ": " + person[x])
-}
-
-//for each loop 
-
-var names = ["jerry", "tom", "pluto", "micky", "mini"];
-names.forEach(function1);
-function function1(currentValue, index) {
-    console.log("Index in array is: " + index + " ::  Value is: " + currentValue);
-}
-
-//
+console.log(myResume);
