0f3cf8998e96b0ba0249de6a0b007f476c77221e
Suresh Kumar S <113002054+surae12@users.noreply.github.com>
2022-12-11 21:23:00 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,50 @@
+let myResume = {
+    "basics": {
+        "name": "Suresh Kumar S",
+        "email": "suricekmar12@gmail.com",
+        "phone": 7010755787,
+        "degree": "B.E",
+        "location": {
+            "address": "15/1-9A krishnappa Nagar",
+            "postalCode": 635109,
+            "city": "hosur",
+            "state": "Tamilnadu",
+            "country": "India"
+        },
+        "profiles": [
+            {
+                "website": "www.linkedin.com/in/suresh-kumar-s-2570a61a5",
+                "github": "https://github.com/surae12"
+            }
+        ]
+    },
+
+
+    "education": [
+        {
+            "institution": "Pavai College Of Technology",
+            "department": "Civil",
+            "studyType": "fulltime",
+            "batch start year": 2015,
+            "batch end year": 2020,
+            "cgpa": 6.25,
+        }
+    ],
+    "skills": [
+        {
+            "name": "html,css,Reactjs,javascript",
+            "level": "beginner",
+        }
+    ],
+    "languages": [
+        {
+            "language": "Tamil,English",
+        }
+    ],
+    "interests": [
+        {
+            "name": "coding,finding solutions,cooking,playing,",
+        }
+    ]
+}
+console.log(myResume);
